8cc9dc2c242ad58f421637c50f7ae20987656b0e
Johan Larsson <johan_gavle1@hotmail.com>
2017-10-10 22:53:38 +0200

    Fix a couple of tests.

--- PropertyChangedAnalyzers.Analyzers/Helpers/KnownSymbols/CaliburnMicroPropertyChangedBase.cs
+++ PropertyChangedAnalyzers.Analyzers/Helpers/KnownSymbols/CaliburnMicroPropertyChangedBase.cs
@@ -3,11 +3,13 @@ namespace PropertyChangedAnalyzers
     internal class CaliburnMicroPropertyChangedBase : QualifiedType
     {
         internal readonly QualifiedMethod Set;
+        internal readonly QualifiedMethod NotifyOfPropertyChange;
 
         internal CaliburnMicroPropertyChangedBase()
             : base("Caliburn.Micro.PropertyChangedBase")
         {
             this.Set = new QualifiedMethod(this, nameof(this.Set));
+            this.NotifyOfPropertyChange = new QualifiedMethod(this, nameof(this.NotifyOfPropertyChange));
         }
     }
 }
--- PropertyChangedAnalyzers.Analyzers/Helpers/KnownSymbols/MvvmLightViewModelBase.cs
+++ PropertyChangedAnalyzers.Analyzers/Helpers/KnownSymbols/MvvmLightViewModelBase.cs
@@ -3,11 +3,13 @@ namespace PropertyChangedAnalyzers
     internal class MvvmLightViewModelBase : QualifiedType
     {
         internal readonly QualifiedMethod Set;
+        internal readonly QualifiedMethod RaisePropertyChanged;
 
         internal MvvmLightViewModelBase()
             : base("GalaSoft.MvvmLight.ViewModelBase")
         {
             this.Set = new QualifiedMethod(this, nameof(this.Set));
+            this.RaisePropertyChanged = new QualifiedMethod(this, nameof(this.RaisePropertyChanged));
         }
     }
 }
