4c8b7fc6afaa8fe51f7454f4906885abcd384413
Johan Larsson <johan_gavle1@hotmail.com>
2017-10-11 19:11:01 +0200

    More tests.

---
+++ PropertyChangedAnalyzers.Test/Helpers/PropertyChangedTest.TryGetInvokedPropertyChangedName.cs
@@ -0,0 +1,90 @@
+﻿namespace PropertyChangedAnalyzers.Test
+{
+    using System.Threading;
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using NUnit.Framework;
+
+    internal partial class PropertyChangedTest
+    {
+        internal class TryGetInvokedPropertyChangedName
+        {
+            [TestCase("this.OnPropertyChanged()")]
+            [TestCase("this.OnPropertyChanged(\"Bar\"")]
+            [TestCase("this.OnPropertyChanged(nameof(Bar)")]
+            [TestCase("this.OnPropertyChanged(nameof(this.Bar)")]
+            [TestCase("this.OnPropertyChanged(() => Bar")]
+            [TestCase("this.OnPropertyChanged(() => this.Bar")]
+            [TestCase("this.OnPropertyChanged(new PropertyChangedEventArgs(\"Bar\")")]
+            [TestCase("this.OnPropertyChanged(new PropertyChangedEventArgs(nameof(Bar))")]
+            [TestCase("this.OnPropertyChanged(new PropertyChangedEventArgs(nameof(this.Bar))")]
+            public void WhenTrue(string signature)
+            {
+                var syntaxTree = CSharpSyntaxTree.ParseText(
+                    @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq.Expressions;
+    using System.Runtime.CompilerServices;
+
+    public class Foo : INotifyPropertyChanged
+    {
+        private int bar;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Bar
+        {
+            get
+            {
+                return this.bar;
+            }
+
+            set
+            {
+                if (value == this.bar)
+                {
+                    return;
+                }
+
+                this.bar = value;
+                this.OnPropertyChanged();
+                this.OnPropertyChanged(""Bar"");
+                this.OnPropertyChanged(nameof(Bar));
+                this.OnPropertyChanged(nameof(this.Bar));
+                this.OnPropertyChanged(() => Bar);
+                this.OnPropertyChanged(() => this.Bar);
+                this.OnPropertyChanged(new PropertyChangedEventArgs(""Bar""));
+                this.OnPropertyChanged(new PropertyChangedEventArgs(nameof(Bar)));
+                this.OnPropertyChanged(new PropertyChangedEventArgs(nameof(this.Bar)));
+            }
+        }
+
+        protected virtual void OnPropertyChanged<T>(Expression<Func<T>> property)
+        {
+            this.OnPropertyChanged(((MemberExpression)property.Body).Member.Name);
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            this.PropertyChanged?.Invoke(this, e);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}");
+                var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+                var semanticModel = compilation.GetSemanticModel(syntaxTree);
+                var invocation = syntaxTree.FindBestMatch<InvocationExpressionSyntax>(signature);
+                Assert.AreEqual(AnalysisResult.Yes, PropertyChanged.TryGetInvokedPropertyChangedName(invocation, semanticModel, CancellationToken.None, out var arg, out var name));
+                Assert.AreEqual("Bar", name);
+            }
+        }
+    }
+}
