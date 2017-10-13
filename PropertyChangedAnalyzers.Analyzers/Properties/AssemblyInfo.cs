0e868d88c56c451cf96f4fedd7b5f8398c026ad5
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-13 17:24:52 +0200

    Bump version.

--- PropertyChangedAnalyzers.Analyzers/Properties/AssemblyInfo.cs
+++ PropertyChangedAnalyzers.Analyzers/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.0.6")]
-[assembly: AssemblyFileVersion("0.1.0.6")]
+[assembly: AssemblyVersion("0.1.0.7")]
+[assembly: AssemblyFileVersion("0.1.0.7")]
 
 [assembly: InternalsVisibleTo("PropertyChangedAnalyzers.CodeFixes")]
 [assembly: InternalsVisibleTo("PropertyChangedAnalyzers.Test")]
