80a5e850ec3482728b28c3f8d80c7367a754a95d
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-13 13:42:27 +0200

    Fix dependencies.

---
+++ PropertyChangedAnalyzers.Test/RunOn.cs
@@ -0,0 +1,25 @@
+﻿namespace PropertyChangedAnalyzers.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using NUnit.Framework;
+
+    public class RunOn
+    {
+        private static readonly IReadOnlyList<DiagnosticAnalyzer> AllAnalyzers = typeof(AnalyzerConstants)
+            .Assembly
+            .GetTypes()
+            .Where(typeof(DiagnosticAnalyzer).IsAssignableFrom)
+            .Select(t => (DiagnosticAnalyzer) Activator.CreateInstance(t))
+            .ToArray();
+
+        [TestCaseSource(nameof(AllAnalyzers))]
+        public void PropertyChangedAnalyzersSln(DiagnosticAnalyzer analyzer)
+        {
+            AnalyzerAssert.Valid(analyzer, CodeFactory.FindSolutionFile("PropertyChangedAnalyzers.sln"));
+        }
+    }
+}
