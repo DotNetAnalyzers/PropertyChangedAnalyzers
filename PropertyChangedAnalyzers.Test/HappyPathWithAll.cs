23bf51c589337739da24ebb519f52c959b36d56d
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-15 13:53:07 +0200

    Recursion tests.

--- PropertyChangedAnalyzers.Test/HappyPathWithAll.cs
+++ PropertyChangedAnalyzers.Test/HappyPathWithAll.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Threading.Tasks;
     using Gu.Roslyn.Asserts;
     using Microsoft.CodeAnalysis.Diagnostics;
 
@@ -238,6 +239,150 @@ internal static class BooleanBoxes
         }
     }";
             AnalyzerAssert.Valid(analyzer, fooCode, fooControlCode, booleanBoxesCode);
+        }
+
+        [TestCaseSource(nameof(AllAnalyzers))]
+        public async Task RecursionInSet(DiagnosticAnalyzer analyzer)
+        {
+            var viewModelBaseCode = @"
+namespace RoslynSandbox.Core
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetValue<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            return this.SetValue(ref field, newValue, propertyName);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}";
+
+            var testCode = @"
+namespace RoslynSandbox.Client
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class Foo : RoslynSandbox.Core.ViewModelBase
+    {
+        private int value2;
+
+        public int Value1 { get; set; }
+
+        public int Value2
+        {
+            get { return this.value2; }
+            set { this.SetValue(ref this.value2, value); }
+        }
+    }
+}";
+            await Analyze.GetDiagnosticsAsync(analyzer, new[] { viewModelBaseCode, testCode }, AnalyzerAssert.MetadataReferences).ConfigureAwait(false);
+        }
+
+        [TestCaseSource(nameof(AllAnalyzers))]
+        public async Task RecursionInOnPropertyChanged(DiagnosticAnalyzer analyzer)
+        {
+            var viewModelBaseCode = @"
+namespace RoslynSandbox.Core
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.OnPropertyChanged(propertyName);
+        }
+    }
+}";
+
+            var testCode = @"
+namespace RoslynSandbox.Client
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class Foo : RoslynSandbox.Core.ViewModelBase
+    {
+        private int value2;
+
+        public int Value1 { get; set; }
+
+        public int Value2
+        {
+            get
+            {
+                return this.value2;
+            }
+
+            set
+            {
+                if (value == this.value2)
+                {
+                    return;
+                }
+
+                this.value2 = value;
+                this.OnPropertyChanged();
+            }
+        }
+    }
+}";
+            await Analyze.GetDiagnosticsAsync(analyzer, new[] { viewModelBaseCode, testCode }, AnalyzerAssert.MetadataReferences).ConfigureAwait(false);
+        }
+
+        [TestCaseSource(nameof(AllAnalyzers))]
+        public async Task RecursiveProperty(DiagnosticAnalyzer analyzer)
+        {
+            var testCode = @"
+namespace RoslynSandbox.Client
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class Foo
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Value
+        {
+            get
+            {
+                return this.Value;
+            }
+
+            set
+            {
+                if (value == this.Value)
+                {
+                    return;
+                }
+
+                this.Value = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}";
+            await Analyze.GetDiagnosticsAsync(analyzer, new[] { testCode }, AnalyzerAssert.MetadataReferences).ConfigureAwait(false);
         }
     }
 }
