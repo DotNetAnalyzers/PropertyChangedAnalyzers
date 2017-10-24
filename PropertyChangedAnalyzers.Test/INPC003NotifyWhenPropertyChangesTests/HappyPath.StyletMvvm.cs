31156064966f62b1f26c10110c9dd2d7e832f99c
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-24 12:25:24 +0200

    INPC003 tests for Stylet

---
+++ PropertyChangedAnalyzers.Test/INPC003NotifyWhenPropertyChangesTests/HappyPath.StyletMvvm.cs
@@ -0,0 +1,152 @@
+namespace PropertyChangedAnalyzers.Test.INPC003NotifyWhenPropertyChangesTests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    internal partial class HappyPath
+    {
+        internal class StyletMvvm
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                AnalyzerAssert.MetadataReferences.Add(SpecialMetadataReferences.Stylet);
+            }
+
+            [OneTimeTearDown]
+            public void TearDown()
+            {
+                AnalyzerAssert.ResetAll();
+            }
+
+            [Test]
+            public void SetProperty()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.SetAndNotify(ref this.name, value) }
+        }
+    }
+}";
+                AnalyzerAssert.Valid<INPC003NotifyWhenPropertyChanges>(testCode);
+            }
+
+            [Test]
+            public void SetPropertyExpressionBodies()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get => return this.name;
+            set => this.SetAndNotify(ref this.name, value)
+        }
+    }
+}";
+                AnalyzerAssert.Valid<INPC003NotifyWhenPropertyChanges>(testCode);
+            }
+
+            [Test]
+            public void SetAffectsCalculatedProperty()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetAndNotify(ref this.name, value))
+                {
+                    this.NotifyOfPropertyChange(nameof(Greeting));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.Valid<INPC003NotifyWhenPropertyChanges>(testCode);
+            }
+
+            [Test]
+            public void SetAffectsCalculatedPropertyExpression()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private int name;
+
+        public string Greeting => $""Hello{this.Name}"";
+
+        public int Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetAndNotify(ref this.name, value))
+                {
+                    this.NotifyOfPropertyChange(() => this.Greeting);
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.Valid<INPC003NotifyWhenPropertyChanges>(testCode);
+            }
+
+            [Test]
+            public void WhenOverriddenSetAndNotify()
+            {
+                var fooBaseCode = @"
+namespace RoslynSandbox
+{
+    public abstract class FooBase : Stylet.PropertyChangedBase
+    {
+        public override bool Set<T>(ref T oldValue, T newValue,[System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            return base.SetAndNotify(ref oldValue, newValue, propertyName);
+        }
+    }
+}";
+
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : FooBase
+    {
+        private int value;
+
+        public int Value
+        {
+            get { return this.value; }
+            set { this.SetAndNotify(ref this.value, value); }
+        }
+    }
+}";
+
+                AnalyzerAssert.Valid<INPC003NotifyWhenPropertyChanges>(fooBaseCode, testCode);
+            }
+        }
+    }
+}
