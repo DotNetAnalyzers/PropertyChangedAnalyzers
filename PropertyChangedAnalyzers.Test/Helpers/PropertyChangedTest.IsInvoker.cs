9dce5c5345cf3794df657711da22a475452fd4f1
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-24 12:12:52 +0200

    More tests.

--- PropertyChangedAnalyzers.Test/Helpers/PropertyChangedTest.IsInvoker.cs
+++ PropertyChangedAnalyzers.Test/Helpers/PropertyChangedTest.IsInvoker.cs
@@ -1,14 +1,128 @@
 ﻿namespace PropertyChangedAnalyzers.Test
 {
+    using System.Linq;
     using System.Threading;
     using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using NUnit.Framework;
 
     internal partial class PropertyChangedTest
     {
         internal class IsInvoker
         {
+            [Test]
+            public void Stylet()
+            {
+                var syntaxTree = CSharpSyntaxTree.ParseText(
+                    @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        private int bar;
+
+        public int Bar
+        {
+            get => this.bar;
+            set
+            {
+                if (value == this.bar)
+                {
+                    return;
+                }
+
+                this.bar = value;
+                this.NotifyOfPropertyChange();
+            }
+        }
+    }
+}");
+                var compilation = CSharpCompilation.Create(
+                    "test",
+                    new[] { syntaxTree },
+                    MetadataReferences.FromAttributes().Concat(new[] { SpecialMetadataReferences.Stylet }));
+                var semanticModel = compilation.GetSemanticModel(syntaxTree);
+                var invocation = syntaxTree.FindBestMatch<InvocationExpressionSyntax>("NotifyOfPropertyChange");
+                var method = (IMethodSymbol)semanticModel.GetSymbolSafe(invocation, CancellationToken.None);
+                Assert.AreEqual(AnalysisResult.Yes, PropertyChanged.IsInvoker(method, semanticModel, CancellationToken.None));
+            }
+
+            [Test]
+            public void CaliburnMicro()
+            {
+                var syntaxTree = CSharpSyntaxTree.ParseText(
+                    @"
+namespace RoslynSandbox
+{
+    public class Foo : Caliburn.Micro.PropertyChangedBase
+    {
+        private int bar;
+
+        public int Bar
+        {
+            get => this.bar;
+            set
+            {
+                if (value == this.bar)
+                {
+                    return;
+                }
+
+                this.bar = value;
+                this.NotifyOfPropertyChange();
+            }
+        }
+    }
+}");
+                var compilation = CSharpCompilation.Create(
+                    "test",
+                    new[] { syntaxTree },
+                    MetadataReferences.Transitive(typeof(Caliburn.Micro.PropertyChangedBase).Assembly));
+                var semanticModel = compilation.GetSemanticModel(syntaxTree);
+                var invocation = syntaxTree.FindBestMatch<InvocationExpressionSyntax>("NotifyOfPropertyChange");
+                var method = (IMethodSymbol)semanticModel.GetSymbolSafe(invocation, CancellationToken.None);
+                Assert.AreEqual(AnalysisResult.Yes, PropertyChanged.IsInvoker(method, semanticModel, CancellationToken.None));
+            }
+
+            [Test]
+            public void MvvmLight()
+            {
+                var syntaxTree = CSharpSyntaxTree.ParseText(
+                    @"
+namespace RoslynSandbox
+{
+    public class Foo : GalaSoft.MvvmLight.ViewModelBase
+    {
+        private int bar;
+
+        public int Bar
+        {
+            get => this.bar;
+            set
+            {
+                if (value == this.bar)
+                {
+                    return;
+                }
+
+                this.bar = value;
+                this.RaisePropertyChanged();
+            }
+        }
+    }
+}");
+                var compilation = CSharpCompilation.Create(
+                    "test",
+                    new[] { syntaxTree },
+                    MetadataReferences.Transitive(typeof(GalaSoft.MvvmLight.ViewModelBase).Assembly));
+                var semanticModel = compilation.GetSemanticModel(syntaxTree);
+                var invocation = syntaxTree.FindBestMatch<InvocationExpressionSyntax>("RaisePropertyChanged");
+                var method = (IMethodSymbol)semanticModel.GetSymbolSafe(invocation, CancellationToken.None);
+                Assert.AreEqual(AnalysisResult.Yes, PropertyChanged.IsInvoker(method, semanticModel, CancellationToken.None));
+            }
+
             [TestCase("protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)")]
             [TestCase("protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)")]
             [TestCase("protected virtual void OnPropertyChanged<T>(Expression<Func<T>> property)")]
