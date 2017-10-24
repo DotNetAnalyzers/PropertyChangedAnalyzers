c42b67c3039d5f0a39248f201af79018629aa876
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-24 10:56:09 +0200

    INPC001 support Stylet.

---
+++ PropertyChangedAnalyzers.Test/Helpers/Stylet.cs
@@ -0,0 +1,23 @@
+﻿namespace PropertyChangedAnalyzers.Test
+{
+    using System.IO;
+    using System.Linq;
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis;
+
+    internal class SpecialMetadataReferences
+    {
+        /// <summary>
+        /// This is needed as stylet is not signed.
+        /// </summary>
+        internal static MetadataReference Stylet { get; } = CreateStyletReference();
+
+        private static MetadataReference CreateStyletReference()
+        {
+            var dll = CodeFactory.FindSolutionFile("PropertyChangedAnalyzers.sln")
+                                      .Directory.EnumerateFiles("Stylet.dll", SearchOption.AllDirectories)
+                                      .First();
+            return MetadataReference.CreateFromFile(dll.FullName);
+        }
+    }
+}
---
+++ PropertyChangedAnalyzers.Test/INPC001ImplementINotifyPropertyChangedTests/CodeFix.StyletMvvm.cs
@@ -0,0 +1,76 @@
+﻿namespace PropertyChangedAnalyzers.Test.INPC001ImplementINotifyPropertyChangedTests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    internal partial class CodeFix
+    {
+        internal class StyletMvvm
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                AnalyzerAssert.MetadataReferences.Add(SpecialMetadataReferences.Stylet);
+            }
+
+            [OneTimeTearDown]
+            public void TearDown()
+            {
+                AnalyzerAssert.ResetAll();
+            }
+
+            [Test]
+            public void SubclassPropertyChangedBase()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ↓Foo
+    {
+        public int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        public int Bar { get; set; }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC001ImplementINotifyPropertyChanged, ImplementINotifyPropertyChangedCodeFixProvider>(testCode, fixedCode, "Subclass Stylet.PropertyChangedBase", AllowCompilationErrors.Yes);
+            }
+
+            [Test]
+            public void ImplementINotifyPropertyChanged()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ↓Foo
+    {
+        public int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : System.ComponentModel.INotifyPropertyChanged
+    {
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        public int Bar { get; set; }
+
+        protected virtual void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC001ImplementINotifyPropertyChanged, ImplementINotifyPropertyChangedCodeFixProvider>(testCode, fixedCode, "Implement INotifyPropertyChanged.", AllowCompilationErrors.Yes);
+            }
+        }
+    }
+}
