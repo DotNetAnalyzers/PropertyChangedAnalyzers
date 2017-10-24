3b2eb2ec0a91cba3de734c2f5fa8f2d2c282dc51
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-24 12:08:25 +0200

    INPC002 handle stylet.

---
+++ PropertyChangedAnalyzers.Test/INPC002MutablePublicPropertyShouldNotifyTests/HappyPath.StyletMvvm.cs
@@ -0,0 +1,96 @@
+﻿namespace PropertyChangedAnalyzers.Test.INPC002MutablePublicPropertyShouldNotifyTests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    internal partial class HappyPath
+    {
+        internal class StyletMvvm
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                AnalyzerAssert.MetadataReferences.Add(SpecialMetadataReferences.Stylet);
+            }
+
+            [OneTimeTearDown]
+            public void TearDown()
+            {
+                AnalyzerAssert.ResetAll();
+            }
+
+            [Test]
+            public void Set()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        private int value;
+
+        public int Value
+        {
+            get { return value; }
+            set { this.Set(ref this.value, value); }
+        }
+    }
+}";
+
+                AnalyzerAssert.Valid<INPC002MutablePublicPropertyShouldNotify>(testCode);
+            }
+
+            [Test]
+            public void SetExpressionBodies()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        private int value;
+
+        public int Value
+        {
+            get => return value;
+            set => this.SetValue(ref this.value, value);
+        }
+    }
+}";
+
+                AnalyzerAssert.Valid<INPC002MutablePublicPropertyShouldNotify>(testCode);
+            }
+
+            [TestCase("null")]
+            [TestCase("string.Empty")]
+            [TestCase(@"""Bar""")]
+            [TestCase(@"nameof(Bar)")]
+            [TestCase(@"nameof(this.Bar)")]
+            public void RaisePropertyChanged(string propertyName)
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private int bar;
+
+        public int Bar
+        {
+            get { return this.bar; }
+            set
+            {
+                if (value == this.bar) return;
+                this.bar = value;
+                this.NotifyOfPropertyChange(nameof(Bar));
+            }
+        }
+    }
+}";
+
+                testCode = testCode.AssertReplace(@"nameof(Bar)", propertyName);
+                AnalyzerAssert.Valid<INPC002MutablePublicPropertyShouldNotify>(testCode);
+            }
+        }
+    }
+}
