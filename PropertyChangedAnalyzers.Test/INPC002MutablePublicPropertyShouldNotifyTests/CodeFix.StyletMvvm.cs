3c3ab1179a92069847d63b300c3c48ac490dd140
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-24 12:08:25 +0200

    INPC002 handle stylet.

---
+++ PropertyChangedAnalyzers.Test/INPC002MutablePublicPropertyShouldNotifyTests/CodeFix.StyletMvvm.cs
@@ -0,0 +1,324 @@
+﻿namespace PropertyChangedAnalyzers.Test.INPC002MutablePublicPropertyShouldNotifyTests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    internal partial class CodeFix
+    {
+        internal class StyletMvvm
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                AnalyzerAssert.MetadataReferences.Add(SpecialMetadataReferences.Stylet);
+            }
+
+            [OneTimeTearDown]
+            public void TearDown()
+            {
+                AnalyzerAssert.ResetAll();
+            }
+
+            [Test]
+            public void AutoPropertyToNotifyWhenValueChanges()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        ↓public int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        private int bar;
+
+        public int Bar
+        {
+            get => this.bar;
+            set
+            {
+                if (value == this.bar)
+                {
+                    return;
+                }
+
+                this.bar = value;
+                this.NotifyOfPropertyChange();
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "Notify when value changes.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "Notify when value changes.");
+            }
+
+            [Test]
+            public void AutoPropertyToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        ↓public int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        private int bar;
+
+        public int Bar
+        {
+            get => this.bar;
+            set => this.SetAndNotify(ref this.bar, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+            }
+
+            [Test]
+            public void InternalClassInternalPropertyAutoPropertyToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    internal class Foo : Stylet.PropertyChangedBase
+    {
+        ↓internal int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    internal class Foo : Stylet.PropertyChangedBase
+    {
+        private int bar;
+
+        internal int Bar
+        {
+            get => this.bar;
+            set => this.SetAndNotify(ref this.bar, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+            }
+
+            [Test]
+            public void AutoPropertyInitializedToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        ↓public int Bar { get; set; } = 1;
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        private int bar = 1;
+
+        public int Bar
+        {
+            get => this.bar;
+            set => this.SetAndNotify(ref this.bar, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+            }
+
+            [Test]
+            public void AutoPropertyVirtualToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        ↓public virtual int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        private int bar;
+
+        public virtual int Bar
+        {
+            get => this.bar;
+            set => this.SetAndNotify(ref this.bar, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+            }
+
+            [Test]
+            public void AutoPropertyPrivateSetToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        ↓public int Bar { get; private set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        private int bar;
+
+        public int Bar
+        {
+            get => this.bar;
+            private set => this.SetAndNotify(ref this.bar, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+            }
+
+            [Test]
+            public void AutoPropertyToSetUnderscoreNames()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        public Foo(int bar)
+        {
+            Bar = bar;
+        }
+
+        ↓public int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Stylet.PropertyChangedBase
+    {
+        private int _bar;
+
+        public Foo(int bar)
+        {
+            Bar = bar;
+        }
+
+        public int Bar
+        {
+            get => _bar;
+            set => SetAndNotify(ref _bar, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+            }
+
+            [Test]
+            public void WithBackingFieldToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        ↓public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.SetAndNotify(ref this.name, value); }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+            }
+
+            [Test]
+            public void WithBackingFieldToSetUnderscoreNames()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string _name;
+
+        ↓public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { SetAndNotify(ref _name, value); }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "PropertyChangedBase.SetAndNotify.");
+            }
+        }
+    }
+}
