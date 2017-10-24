19975611b2791f4f9b004f7fc0a5cef46a78dff9
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-24 12:32:29 +0200

    INPC005 tests for Stylet

---
+++ PropertyChangedAnalyzers.Test/INPC005CheckIfDifferentBeforeNotifyingTests/CodeFix.StyletMvvm.cs
@@ -0,0 +1,291 @@
+﻿namespace PropertyChangedAnalyzers.Test.INPC005CheckIfDifferentBeforeNotifyingTests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    internal partial class CodeFix
+    {
+        internal class StyletMvvm
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                AnalyzerAssert.MetadataReferences.Add(SpecialMetadataReferences.Stylet);
+            }
+
+            [OneTimeTearDown]
+            public void TearDown()
+            {
+                AnalyzerAssert.ResetAll();
+            }
+
+            [Test]
+            public void SetAffectsCalculatedProperty()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                this.SetAndNotify(ref this.name, value);
+                ↓this.NotifyOfPropertyChange(nameof(this.Greeting));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetAndNotify(ref this.name, value))
+                {
+                    this.NotifyOfPropertyChange(nameof(this.Greeting));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+            }
+
+            [Test]
+            public void SetAffectsCalculatedPropertyInternalClassInternalProperty()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    internal class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        internal string Greeting => $""Hello {this.Name}"";
+
+        internal string Name
+        {
+            get { return this.name; }
+            set
+            {
+                this.SetAndNotify(ref this.name, value);
+                ↓this.NotifyOfPropertyChange(nameof(this.Greeting));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    internal class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        internal string Greeting => $""Hello {this.Name}"";
+
+        internal string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetAndNotify(ref this.name, value))
+                {
+                    this.NotifyOfPropertyChange(nameof(this.Greeting));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+            }
+
+            [Test]
+            public void SetAffectsCalculatedPropertyEmptyIf()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetAndNotify(ref this.name, value))
+                {
+                }
+
+                ↓this.NotifyOfPropertyChange(nameof(this.Greeting));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetAndNotify(ref this.name, value))
+                {
+                    this.NotifyOfPropertyChange(nameof(this.Greeting));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+            }
+
+            [Test]
+            public void SetAffectsSecondCalculatedProperty()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Greeting1 => $""Hello {this.Name}"";
+
+        public string Greeting2 => $""Hej {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetAndNotify(ref this.name, value))
+                {
+                    this.NotifyOfPropertyChange(nameof(this.Greeting1));
+                }
+
+                ↓this.NotifyOfPropertyChange(nameof(this.Greeting2));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Greeting1 => $""Hello {this.Name}"";
+
+        public string Greeting2 => $""Hej {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetAndNotify(ref this.name, value))
+                {
+                    this.NotifyOfPropertyChange(nameof(this.Greeting1));
+                    this.NotifyOfPropertyChange(nameof(this.Greeting2));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+            }
+
+            [Test]
+            public void SetAffectsSecondCalculatedPropertyMissingBraces()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Greeting1 => $""Hello {this.Name}"";
+
+        public string Greeting2 => $""Hej {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetAndNotify(ref this.name, value))
+                    this.NotifyOfPropertyChange(nameof(this.Greeting1));
+                ↓this.NotifyOfPropertyChange(nameof(this.Greeting2));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Stylet.PropertyChangedBase
+    {
+        private string name;
+
+        public string Greeting1 => $""Hello {this.Name}"";
+
+        public string Greeting2 => $""Hej {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetAndNotify(ref this.name, value))
+                {
+                    this.NotifyOfPropertyChange(nameof(this.Greeting1));
+                    this.NotifyOfPropertyChange(nameof(this.Greeting2));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+            }
+        }
+    }
+}
