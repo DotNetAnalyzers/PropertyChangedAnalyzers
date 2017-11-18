d7c43413a791cec7e9ee6defcc1fd1e828c2bf52
Johan Larsson <johan_gavle1@hotmail.com>
2017-11-18 19:40:10 +0100

    Tests for INPC002

---
+++ PropertyChangedAnalyzers.Test/INPC002MutablePublicPropertyShouldNotifyTests/CodeFix.PrismBindableBase.cs
@@ -0,0 +1,334 @@
+﻿namespace PropertyChangedAnalyzers.Test.INPC002MutablePublicPropertyShouldNotifyTests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    internal partial class CodeFix
+    {
+        internal class PrismBindableBase
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                AnalyzerAssert.AddTransitiveMetadataReferences(typeof(Microsoft.Practices.Prism.Mvvm.BindableBase).Assembly);
+            }
+
+            [OneTimeTearDown]
+            public void TearDown()
+            {
+                AnalyzerAssert.ResetAll();
+            }
+
+            [Test]
+            public void AutoPropertyToNotifyWhenValueChanges()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        ↓public int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private int bar;
+
+        public int Bar
+        {
+            get => this.bar;
+            set
+            {
+                if (value == this.bar)
+                {
+                    return;
+                }
+
+                this.bar = value;
+                this.OnPropertyChanged(nameof(this.Bar));
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "Notify when value changes.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "Notify when value changes.");
+            }
+
+            [Test]
+            public void AutoPropertyToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        ↓public int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private int bar;
+
+        public int Bar
+        {
+            get => this.bar;
+            set => this.SetProperty(ref this.bar, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+            }
+
+            [Test]
+            public void InternalClassInternalPropertyAutoPropertyToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    internal class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        ↓internal int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    internal class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private int bar;
+
+        internal int Bar
+        {
+            get => this.bar;
+            set => this.SetProperty(ref this.bar, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+            }
+
+            [Test]
+            public void AutoPropertyInitializedToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        ↓public int Bar { get; set; } = 1;
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private int bar = 1;
+
+        public int Bar
+        {
+            get => this.bar;
+            set => this.SetProperty(ref this.bar, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+            }
+
+            [Test]
+            public void AutoPropertyVirtualToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        ↓public virtual int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private int bar;
+
+        public virtual int Bar
+        {
+            get => this.bar;
+            set => this.SetProperty(ref this.bar, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+            }
+
+            [Test]
+            public void AutoPropertyPrivateSetToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        ↓public int Bar { get; private set; }
+
+        public void Mutate()
+        {
+            this.Bar++;
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private int bar;
+
+        public int Bar
+        {
+            get => this.bar;
+            private set => this.SetProperty(ref this.bar, value);
+        }
+
+        public void Mutate()
+        {
+            this.Bar++;
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+            }
+
+            [Test]
+            public void AutoPropertyToSetUnderscoreNames()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        public Foo(int bar)
+        {
+            Bar = bar;
+        }
+
+        ↓public int Bar { get; set; }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class Foo : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private int _bar;
+
+        public Foo(int bar)
+        {
+            Bar = bar;
+        }
+
+        public int Bar
+        {
+            get => _bar;
+            set => SetProperty(ref _bar, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+            }
+
+            [Test]
+            public void WithBackingFieldToSet()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        ↓public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.SetProperty(ref this.name, value); }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+            }
+
+            [Test]
+            public void WithBackingFieldToSetUnderscoreNames()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string _name;
+
+        ↓public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+                AnalyzerAssert.FixAll<INPC002MutablePublicPropertyShouldNotify, MakePropertyNotifyCodeFixProvider>(testCode, fixedCode, "BindableBase.SetProperty.");
+            }
+        }
+    }
+}
