a442b7e8b259a15218852fd36d48e5c850381cc0
Johan Larsson <johan_gavle1@hotmail.com>
2017-11-18 19:57:10 +0100

    Tests for INPC005

---
+++ PropertyChangedAnalyzers.Test/INPC005CheckIfDifferentBeforeNotifyingTests/CodeFix.PrismBindableBase.cs
@@ -0,0 +1,420 @@
+﻿namespace PropertyChangedAnalyzers.Test.INPC005CheckIfDifferentBeforeNotifyingTests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    internal partial class CodeFix
+    {
+        internal class PrismBindableBase
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                AnalyzerAssert.AddTransitiveMetadataReferences(typeof(Microsoft.Practices.Prism.Mvvm.BindableBase).Assembly);
+            }
+
+            [OneTimeTearDown]
+            public void TearDown()
+            {
+                AnalyzerAssert.ResetAll();
+            }
+
+            [Test]
+            public void NoCheckAddIfReturn()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                this.name = value;
+                ↓this.OnPropertyChanged(nameof(this.Name));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                if (value == this.name)
+                {
+                    return;
+                }
+
+                this.name = value;
+                this.OnPropertyChanged(nameof(this.Name));
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode, "Check that value is different before notifying.");
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode, "Check that value is different before notifying.");
+            }
+
+            [Test]
+            public void NoCheckToUseSetAndRaise()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                this.name = value;
+                ↓this.OnPropertyChanged(nameof(this.Name));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get => this.name;
+            set => this.SetProperty(ref this.name, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode, "Use BindableBase.SetProperty");
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode, "Use BindableBase.SetProperty");
+            }
+
+            [Test]
+            public void NoCheckExpressionToUseSetAndRaise()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                this.name = value;
+                ↓this.OnPropertyChanged(() => this.Name);
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get => this.name;
+            set => this.SetProperty(ref this.name, value);
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode, "Use BindableBase.SetProperty");
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode, "Use BindableBase.SetProperty");
+            }
+
+            [Test]
+            public void SetAffectsCalculatedProperty()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                this.SetProperty(ref this.name, value);
+                ↓this.OnPropertyChanged(nameof(this.Greeting));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetProperty(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(this.Greeting));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+            }
+
+            [Test]
+            public void SetAffectsCalculatedPropertyInternalClassInternalProperty()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    internal class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        internal string Greeting => $""Hello {this.Name}"";
+
+        internal string Name
+        {
+            get { return this.name; }
+            set
+            {
+                this.SetProperty(ref this.name, value);
+                ↓this.OnPropertyChanged(nameof(this.Greeting));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    internal class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        internal string Greeting => $""Hello {this.Name}"";
+
+        internal string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetProperty(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(this.Greeting));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+            }
+
+            [Test]
+            public void SetAffectsCalculatedPropertyEmptyIf()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetProperty(ref this.name, value))
+                {
+                }
+
+                ↓this.OnPropertyChanged(nameof(this.Greeting));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetProperty(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(this.Greeting));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+            }
+
+            [Test]
+            public void SetAffectsSecondCalculatedProperty()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Greeting1 => $""Hello {this.Name}"";
+
+        public string Greeting2 => $""Hej {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetProperty(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(this.Greeting1));
+                }
+
+                ↓this.OnPropertyChanged(nameof(this.Greeting2));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Greeting1 => $""Hello {this.Name}"";
+
+        public string Greeting2 => $""Hej {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetProperty(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(this.Greeting1));
+                    this.OnPropertyChanged(nameof(this.Greeting2));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+            }
+
+            [Test]
+            public void SetAffectsSecondCalculatedPropertyMissingBraces()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Greeting1 => $""Hello {this.Name}"";
+
+        public string Greeting2 => $""Hej {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetProperty(ref this.name, value))
+                    this.OnPropertyChanged(nameof(this.Greeting1));
+                ↓this.OnPropertyChanged(nameof(this.Greeting2));
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Greeting1 => $""Hello {this.Name}"";
+
+        public string Greeting2 => $""Hej {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetProperty(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(this.Greeting1));
+                    this.OnPropertyChanged(nameof(this.Greeting2));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.CodeFix<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<INPC005CheckIfDifferentBeforeNotifying, CheckIfDifferentBeforeNotifyFixProvider>(testCode, fixedCode);
+            }
+        }
+    }
+}
