22a952ff26443aeeebaa8c3a3f478fba2ded9b08
Johan Larsson <johan_gavle1@hotmail.com>
2017-11-18 19:48:00 +0100

    Tests for INPC003

---
+++ PropertyChangedAnalyzers.Test/INPC003NotifyWhenPropertyChangesTests/HappyPath.PrismBindableBase.cs
@@ -0,0 +1,119 @@
+namespace PropertyChangedAnalyzers.Test.INPC003NotifyWhenPropertyChangesTests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    internal partial class HappyPath
+    {
+        internal class PrismBindableBase
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                AnalyzerAssert.AddTransitiveMetadataReferences(typeof(Microsoft.Practices.Prism.Mvvm.BindableBase).Assembly);
+            }
+
+            [OneTimeTearDown]
+            public void TearDown()
+            {
+                AnalyzerAssert.ResetAll();
+            }
+
+            [Test]
+            public void SetProperty()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.SetProperty(ref this.name, value) }
+        }
+    }
+}";
+                AnalyzerAssert.Valid(Analyzer, testCode);
+            }
+
+            [Test]
+            public void SetPropertyExpressionBodies()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get => return this.name;
+            set => this.SetProperty(ref this.name, value)
+        }
+    }
+}";
+                AnalyzerAssert.Valid(Analyzer, testCode);
+            }
+
+            [Test]
+            public void SetAffectsCalculatedPropertyNameOf()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetProperty(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(Greeting));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.Valid(Analyzer, testCode);
+            }
+
+            [Test]
+            public void SetAffectsCalculatedPropertyExpression()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public class ViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    {
+        private int name;
+
+        public string Greeting => $""Hello{this.Name}"";
+
+        public int Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.SetProperty(ref this.name, value))
+                {
+                    this.OnPropertyChanged(() => this.Greeting);
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.Valid(Analyzer, testCode);
+            }
+        }
+    }
+}
