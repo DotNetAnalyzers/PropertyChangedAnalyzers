11426d929595bcf174b86c65cf7b760388aa7d04
Johan Larsson <johan_gavle1@hotmail.com>
2017-11-22 00:15:48 +0100

    Failing tests for INPC014
    (cherry picked from commit 57ecf869d8a7a4dc34d7d739c5a7a42c8e99e135)

--- PropertyChangedAnalyzers.Test/INPC014PreferSettingBackingFieldInCtorTests/HappyPath.cs
+++ PropertyChangedAnalyzers.Test/INPC014PreferSettingBackingFieldInCtorTests/HappyPath.cs
@@ -85,7 +85,7 @@ namespace RoslynSandbox
         }
 
         [Test]
-        public void IgnoreWhenValidation()
+        public void IgnoreWhenValidationThrows()
         {
             var testCode = @"
 namespace RoslynSandbox
@@ -120,6 +120,110 @@ namespace RoslynSandbox
                 }
 
                 this.value = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}";
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [Test]
+        public void IgnoreWhenValidation()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Diagnostics;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private int value;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel(int value)
+        {
+            this.Value = value;
+        }
+
+        public int Value
+        {
+            get => this.value;
+            set
+            {
+                GreaterThan(value, 0, nameof(value));
+                if (value == this.value)
+                {
+                    return;
+                }
+
+                this.value = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private static void GreaterThan<T>(T value, T min, string parameterName)
+            where T : IComparable<T>
+        {
+            Debug.Assert(!string.IsNullOrEmpty(parameterName), $""{ nameof(parameterName)}
+            cannot be null"");
+            if (Comparer<T>.Default.Compare(value, min) <= 0)
+            {
+                string message = $""Expected {parameterName} to be greater than {min}, {parameterName} was {value}"";
+                throw new ArgumentException(message, parameterName);
+            }
+        }
+    }
+}";
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [Test]
+        public void IgnoreWhenSideEffect()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System.ComponentModel;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private int value;
+        private int count;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel(int value)
+        {
+            this.Value = value;
+        }
+
+        public int Value
+        {
+            get => this.value;
+            set
+            {
+                if (value == this.value)
+                {
+                    return;
+                }
+
+                this.value = value;
+                this.count++;
                 this.OnPropertyChanged();
             }
         }
