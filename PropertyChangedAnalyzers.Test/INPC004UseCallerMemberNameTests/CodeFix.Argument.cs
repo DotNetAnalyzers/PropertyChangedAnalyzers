59f2a62d0b0a744e01abd998db1a2b20e706b6b9
JohanLarsson <johan_gavle1@hotmail.com>
2018-03-21 14:49:34 +0100

    Test use CallerMemberName when calling SetMethod.

--- PropertyChangedAnalyzers.Test/INPC004UseCallerMemberNameTests/CodeFix.Argument.cs
+++ PropertyChangedAnalyzers.Test/INPC004UseCallerMemberNameTests/CodeFix.Argument.cs
@@ -520,6 +520,94 @@ namespace RoslynSandbox
 }";
                 AnalyzerAssert.FixAll(Analyzer, Fix, ExpectedDiagnostic, testCode, fixedCode);
             }
+
+            [Test]
+            public void SetAffectsCalculatedPropertyExplicitNameOf()
+            {
+                var viewModelBaseCode = @"
+namespace RoslynSandbox.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq.Expressions;
+    using System.Runtime.CompilerServices;
+
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual bool TrySet<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected virtual void OnPropertyChanged<T>(Expression<Func<T>> property)
+        {
+            this.OnPropertyChanged(((MemberExpression)property.Body).Member.Name);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}";
+
+                var testCode = @"
+namespace RoslynSandbox.Client
+{
+    public class ViewModel : RoslynSandbox.Core.ViewModelBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.TrySet(ref this.name, value, ↓nameof(Name)))
+                {
+                    this.OnPropertyChanged(nameof(Greeting));
+                }
+            }
+        }
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox.Client
+{
+    public class ViewModel : RoslynSandbox.Core.ViewModelBase
+    {
+        private string name;
+
+        public string Greeting => $""Hello {this.Name}"";
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.TrySet(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(Greeting));
+                }
+            }
+        }
+    }
+}";
+                AnalyzerAssert.FixAll(Analyzer, Fix, ExpectedDiagnostic, new[] { viewModelBaseCode, testCode }, fixedCode);
+            }
         }
     }
 }
