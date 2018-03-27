a27dbd711666c503ebee692f2eb54688981f26b0
JohanLarsson <johan_gavle1@hotmail.com>
2018-03-27 14:03:29 +0200

    ReproBox

---
+++ PropertyChangedAnalyzers.Test/ReproBox.cs
@@ -0,0 +1,47 @@
+namespace PropertyChangedAnalyzers.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using NUnit.Framework;
+    using PropertyChangedAnalyzers;
+
+    [Explicit("For harvesting testcases only.")]
+    public class ReproBox
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static readonly IReadOnlyList<DiagnosticAnalyzer> AllAnalyzers =
+            typeof(KnownSymbol).Assembly.GetTypes()
+                               .Where(typeof(DiagnosticAnalyzer).IsAssignableFrom)
+                               .Select(t => (DiagnosticAnalyzer)Activator.CreateInstance(t))
+                               .ToArray();
+
+        private static readonly Solution Solution = CodeFactory.CreateSolution(
+            new FileInfo("C:\\Tfs\\Map\\MAP\\Map.sln"),
+            AllAnalyzers,
+            AnalyzerAssert.MetadataReferences);
+
+        [TestCaseSource(nameof(AllAnalyzers))]
+        public void SolutionRepro(DiagnosticAnalyzer analyzer)
+        {
+            AnalyzerAssert.Valid(analyzer, Solution);
+        }
+
+        [TestCaseSource(nameof(AllAnalyzers))]
+        public void Repro(DiagnosticAnalyzer analyzer)
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    public class Fo
+    {
+    }
+}";
+            AnalyzerAssert.Valid(analyzer, testCode);
+        }
+    }
+}
