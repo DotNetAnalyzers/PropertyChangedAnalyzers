22c9745e9d1194d3d090dbecb949e9d2064fb6a6
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-21 19:06:45 +0200

    ValidCode.InheritanceTrySet

---
+++ ValidCode/InheritanceTrySet/ExpressionBodies.cs
@@ -0,0 +1,29 @@
+namespace ValidCode.InheritanceTrySet
+{
+    public sealed class ExpressionBodies : ExpressionBodiesViewModelBase
+    {
+        private string name;
+        private int value;
+
+        public string Greeting => $"Hello {this.name}";
+
+        public string Name
+        {
+            get => this.name;
+
+            set
+            {
+                if (this.TrySet(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(this.Greeting));
+                }
+            }
+        }
+
+        public int Value
+        {
+            get => this.value;
+            set => this.TrySet(ref this.value, value);
+        }
+    }
+}
---
+++ ValidCode/InheritanceTrySet/StatementBodies.cs
@@ -0,0 +1,37 @@
+// ReSharper disable All
+namespace ValidCode.InheritanceTrySet
+{
+    public sealed class StatementBodies : StatementBodiesViewModelBase
+    {
+        private string name;
+        private int value;
+
+        public string Greeting => $"Hello {this.name}";
+
+        public string Name
+        {
+            get => this.name;
+
+            set
+            {
+                if (this.TrySet(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(this.Greeting));
+                }
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return this.value;
+            }
+
+            set
+            {
+                this.TrySet(ref this.value, value);
+            }
+        }
+    }
+}
