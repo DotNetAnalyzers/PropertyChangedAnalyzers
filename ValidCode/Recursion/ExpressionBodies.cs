7ac59862d540db438d8283e544dff13f5ae0a604
JohanLarsson <johan_gavle1@hotmail.com>
2019-08-21 17:35:40 +0200

    Test recursion.
    Close #129.

---
+++ ValidCode/Recursion/ExpressionBodies.cs
@@ -0,0 +1,40 @@
+// ReSharper disable All
+#pragma warning disable INPC007, INPC015
+namespace ValidCode.Recursion
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public sealed class ExpressionBodies : INotifyPropertyChanged
+    {
+        private string name;
+        private int value;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Greeting => this.Greeting;
+
+        public string Name
+        {
+            get => this.Name;
+
+            set
+            {
+                if (this.TrySet(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(this.Greeting));
+                }
+            }
+        }
+
+        public int Value
+        {
+            get => this.Value;
+            set => this.TrySet(ref this.value, value);
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null) => this.OnPropertyChanged(propertyName);
+
+        private bool TrySet<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null) => this.TrySet(ref field, newValue, propertyName);
+    }
+}
---
+++ ValidCode/Recursion/StatementBodies.cs
@@ -0,0 +1,72 @@
+// ReSharper disable All
+#pragma warning disable INPC007, INPC015, INPC020
+namespace ValidCode.Recursion
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public sealed class StatementnBodies : INotifyPropertyChanged
+    {
+        private string name;
+        private int value;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Greeting1
+        {
+            get
+            {
+                return this.Greeting1;
+            }
+        }
+
+        public string Greeting2
+        {
+            get
+            {
+                return $"{this.Name}";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.Name;
+            }
+
+            set
+            {
+                if (this.TrySet(ref this.name, value))
+                {
+                    this.OnPropertyChanged(nameof(this.Greeting1));
+                    this.OnPropertyChanged(nameof(this.Greeting2));
+                }
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return this.Value;
+            }
+
+            set
+            {
+                this.TrySet(ref this.value, value);
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.OnPropertyChanged(propertyName);
+        }
+
+        private bool TrySet<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            return TrySet(ref field, newValue, propertyName);
+        }
+    }
+}
