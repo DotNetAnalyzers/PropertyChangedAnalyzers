95c24c4929dc8d91ff96ca4d4545c75d58d0ab38
Johan Larsson <johan_gavle1@hotmail.com>
2019-11-05 23:16:35 +0100

    Cleanup

--- PropertyChangedAnalyzers/Analyzers/ClassDeclarationAnalyzer.cs
+++ PropertyChangedAnalyzers/Analyzers/ClassDeclarationAnalyzer.cs
@@ -27,20 +27,10 @@ namespace PropertyChangedAnalyzers
         private static void Handle(SyntaxNodeAnalysisContext context)
         {
             if (!context.IsExcludedFromAnalysis() &&
-                context.ContainingSymbol is INamedTypeSymbol type &&
-                context.Node is ClassDeclarationSyntax classDeclaration)
+                context.ContainingSymbol is INamedTypeSymbol { IsStatic: false } type &&
+                context.Node is ClassDeclarationSyntax classDeclaration &&
+                !IsExcludedType())
             {
-                if (type.IsStatic ||
-                    type.IsAssignableTo(KnownSymbol.INotifyPropertyChanged, context.Compilation) ||
-                    type.IsAssignableTo(KnownSymbol.MarkupExtension, context.Compilation) ||
-                    type.IsAssignableTo(KnownSymbol.Attribute, context.Compilation) ||
-                    type.IsAssignableTo(KnownSymbol.IValueConverter, context.Compilation) ||
-                    type.IsAssignableTo(KnownSymbol.IMultiValueConverter, context.Compilation) ||
-                    type.IsAssignableTo(KnownSymbol.DataTemplateSelector, context.Compilation) ||
-                    type.IsAssignableTo(KnownSymbol.DependencyObject, context.Compilation))
-                {
-                    return;
-                }
 
                 if (classDeclaration.Members.TryFirstOfType(x => Property.ShouldNotify(x, context.SemanticModel, context.CancellationToken), out PropertyDeclarationSyntax _))
                 {
@@ -63,6 +53,17 @@ namespace PropertyChangedAnalyzers
 
                     context.ReportDiagnostic(Diagnostic.Create(Descriptors.INPC001ImplementINotifyPropertyChanged, classDeclaration.Identifier.GetLocation(), $"The class {type.Name} has event PropertyChanged but does not implement INotifyPropertyChanged."));
                 }
+            }
+
+            bool IsExcludedType()
+            {
+                return type.IsAssignableTo(KnownSymbol.INotifyPropertyChanged, context.Compilation) ||
+                       type.IsAssignableTo(KnownSymbol.MarkupExtension, context.Compilation) ||
+                       type.IsAssignableTo(KnownSymbol.Attribute, context.Compilation) ||
+                       type.IsAssignableTo(KnownSymbol.IValueConverter, context.Compilation) ||
+                       type.IsAssignableTo(KnownSymbol.IMultiValueConverter, context.Compilation) ||
+                       type.IsAssignableTo(KnownSymbol.DataTemplateSelector, context.Compilation) ||
+                       type.IsAssignableTo(KnownSymbol.DependencyObject, context.Compilation);
             }
         }
     }
