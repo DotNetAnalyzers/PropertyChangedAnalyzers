11b20347ca30d354104f8df30b15c3e22b33e293
JohanLarsson <johan_gavle1@hotmail.com>
2019-11-06 20:59:55 +0100

    ValidCode.StaticClass

---
+++ ValidCode/StaticClass.cs
@@ -0,0 +1,47 @@
+namespace ValidCode
+{
+    using System.ComponentModel;
+
+    public static class StaticClass
+    {
+        private static string name;
+        private static int number;
+
+        public static event PropertyChangedEventHandler PropertyChanged;
+
+        public static string Name
+        {
+            get => name;
+            set
+            {
+                if (name == value)
+                {
+                    return;
+                }
+
+                name = value;
+                PropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+
+        public static int Number
+        {
+            get => number;
+            set
+            {
+                if(value == number)
+                {
+                    return;
+                }
+
+                number = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private static void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(null, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
