692cfb91900ee50a8c57714884db09506d85faf5
jnm2 <me@jnm2.com>
2019-12-14 14:22:18 -0500

    Add tests showing that autodetection of field prefix and qualification styles is working

--- PropertyChangedAnalyzers.Test/INPC002MutablePublicPropertyShouldNotifyTests/CodeFix.Style.cs
+++ PropertyChangedAnalyzers.Test/INPC002MutablePublicPropertyShouldNotifyTests/CodeFix.Style.cs
@@ -150,6 +150,222 @@ namespace N
         }
 
         [Test]
+        public static void AutoPropertyQualifiedUnderscoreFields()
+        {
+            var before = @"
+namespace N
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class C : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ↓Name { get; set; }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool TrySet<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}";
+
+            var after = @"
+namespace N
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class C : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get => this._name; set => TrySet(ref this._name, value); }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool TrySet<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}";
+            RoslynAssert.CodeFix(Analyzer, Fix, ExpectedDiagnostic, new[] { Code.QualifiedUnderscoreFields, before }, after, fixTitle: "TrySet(ref field, newValue)");
+            RoslynAssert.FixAll(Analyzer, Fix, ExpectedDiagnostic, new[] { Code.QualifiedUnderscoreFields, before }, after, fixTitle: "TrySet(ref field, newValue)");
+        }
+
+        [Test]
+        public static void AutoPropertyUnqualifiedUnprefixedFields()
+        {
+            var before = @"
+namespace N
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class C : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ↓Name { get; set; }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool TrySet<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}";
+
+            var after = @"
+namespace N
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class C : INotifyPropertyChanged
+    {
+        private string name;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get => name; set => TrySet(ref name, value); }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool TrySet<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}";
+            RoslynAssert.CodeFix(Analyzer, Fix, ExpectedDiagnostic, new[] { Code.UnqualifiedUnprefixedFields, before }, after, fixTitle: "TrySet(ref field, newValue)");
+            RoslynAssert.FixAll(Analyzer, Fix, ExpectedDiagnostic, new[] { Code.UnqualifiedUnprefixedFields, before }, after, fixTitle: "TrySet(ref field, newValue)");
+        }
+
+        [Test]
+        public static void AutoPropertyQualifiedUnprefixedFields()
+        {
+            var before = @"
+namespace N
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class C : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ↓Name { get; set; }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool TrySet<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}";
+
+            var after = @"
+namespace N
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class C : INotifyPropertyChanged
+    {
+        private string name;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get => this.name; set => TrySet(ref this.name, value); }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool TrySet<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}";
+            RoslynAssert.CodeFix(Analyzer, Fix, ExpectedDiagnostic, new[] { Code.QualifiedUnprefixedFields, before }, after, fixTitle: "TrySet(ref field, newValue)");
+            RoslynAssert.FixAll(Analyzer, Fix, ExpectedDiagnostic, new[] { Code.QualifiedUnprefixedFields, before }, after, fixTitle: "TrySet(ref field, newValue)");
+        }
+
+        [Test]
         public static void WithBackingFieldToSetExpressionBodiesSeparateLines()
         {
             var before = @"
--- PropertyChangedAnalyzers.Test/TestHelpers/Code.cs
+++ PropertyChangedAnalyzers.Test/TestHelpers/Code.cs
@@ -27,5 +27,83 @@ namespace N
         private int M() => _f;
     }
 }";
+
+        /// <summary>
+        /// A class that uses both underscore field conventions and qualified field access.
+        /// </summary>
+        public const string QualifiedUnderscoreFields = @"
+namespace N
+{
+    using System;
+    class QualifiedUnderscoreFields
+    {
+        private readonly int _f;
+        QualifiedUnderscoreFields()
+        {
+            this._f = 1;
+            P = 2;
+            E?.Invoke();
+            M();
+        }
+
+        public event Action E;
+
+        public int P { get; }
+
+        private int M() => this._f;
+    }
+}";
+
+        /// <summary>
+        /// A class that does not use underscore field conventions but which also does not qualify field access.
+        /// </summary>
+        public const string UnqualifiedUnprefixedFields = @"
+namespace N
+{
+    using System;
+    class UnqualifiedUnprefixedFields
+    {
+        private readonly int f;
+        UnqualifiedUnprefixedFields()
+        {
+            f = 1;
+            P = 2;
+            E?.Invoke();
+            M();
+        }
+
+        public event Action E;
+
+        public int P { get; }
+
+        private int M() => f;
+    }
+}";
+
+        /// <summary>
+        /// A class that does not use underscore field conventions and which qualifies field access.
+        /// </summary>
+        public const string QualifiedUnprefixedFields = @"
+namespace N
+{
+    using System;
+    class QualifiedUnprefixedFields
+    {
+        private readonly int f;
+        QualifiedUnprefixedFields()
+        {
+            this.f = 1;
+            P = 2;
+            E?.Invoke();
+            M();
+        }
+
+        public event Action E;
+
+        public int P { get; }
+
+        private int M() => this.f;
+    }
+}";
     }
 }
