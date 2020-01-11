eace67e3372ce08a10553ca57039fd51d3b488e9
JohanLarsson <johan_gavle1@hotmail.com>
2020-01-11 11:44:09 +0100

    Simplify.

--- PropertyChangedAnalyzers/Helpers/Getter.cs
+++ PropertyChangedAnalyzers/Helpers/Getter.cs
@@ -1,41 +1,27 @@
-namespace PropertyChangedAnalyzers
+﻿namespace PropertyChangedAnalyzers
 {
     using System.Diagnostics.CodeAnalysis;
-    using Gu.Roslyn.AnalyzerExtensions;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
 
     public static class Getter
     {
         internal static bool TrySingleReturned(AccessorDeclarationSyntax getter, [NotNullWhen(true)] out ExpressionSyntax? result)
         {
-            if (getter.ExpressionBody is { } getterExpressionBody)
+            switch (getter)
             {
-                result = getterExpressionBody.Expression;
-                return result != null;
-            }
-
-            if (getter.Body is { } body)
-            {
-                if (body.Statements.Count == 0)
-                {
+                case { ExpressionBody: { Expression: { } expression } }:
+                    result = expression;
+                    return true;
+                case { Body: { Statements: { Count: 1 } statements } }
+                    when statements[0] is ReturnStatementSyntax returnStatement:
+                    result = returnStatement.Expression;
+                    return result != null;
+                case { Body: { } body}:
+                    return ReturnExpressionsWalker.TryGetSingle(body, out result);
+                default:
                     result = null;
                     return false;
-                }
-
-                if (body.Statements.TrySingle(out var statement))
-                {
-                    if (statement is ReturnStatementSyntax returnStatement)
-                    {
-                        result = returnStatement.Expression;
-                        return result != null;
-                    }
-                }
-
-                return ReturnExpressionsWalker.TryGetSingle(getter, out result);
             }
-
-            result = null;
-            return false;
         }
     }
 }
