fa5e30b7e61a6310b6463219701333203cc20f46
Johan Larsson <johan_gavle1@hotmail.com>
2020-11-09 18:47:10 +0100

    Format.

--- PropertyChangedAnalyzers/Helpers/PropertyChangedEventArgs.cs
+++ PropertyChangedAnalyzers/Helpers/PropertyChangedEventArgs.cs
@@ -2,7 +2,9 @@
 {
     using System.Diagnostics.CodeAnalysis;
     using System.Threading;
+
     using Gu.Roslyn.AnalyzerExtensions;
+
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -62,16 +64,19 @@
             {
                 switch (candidate)
                 {
-                    case IFieldSymbol field when field.TrySingleDeclaration(cancellationToken, out var declaration) &&
-                                                 declaration.Declaration.Variables.TryLast(out var variable) &&
-                                                 variable.Initializer is { } initializer:
+                    case IFieldSymbol field
+                        when field.TrySingleDeclaration(cancellationToken, out var declaration) &&
+                             declaration.Declaration.Variables.TryLast(out var variable) &&
+                             variable.Initializer is { } initializer:
                         return TryGetCreation(initializer.Value, out nameArg);
-                    case IPropertySymbol property when property.TrySingleDeclaration(cancellationToken, out PropertyDeclarationSyntax? declaration) &&
-                                                       declaration.Initializer is { } initializer:
+                    case IPropertySymbol property
+                        when property.TrySingleDeclaration(cancellationToken, out PropertyDeclarationSyntax? declaration) &&
+                             declaration.Initializer is { } initializer:
                         return TryGetCreation(initializer.Value, out nameArg);
-                    case ILocalSymbol local when local.TrySingleDeclaration(cancellationToken, out VariableDeclarationSyntax? declaration) &&
-                                                 declaration.Variables.TryLast(out var variable) &&
-                                                 variable.Initializer is { } initializer:
+                    case ILocalSymbol local
+                        when local.TrySingleDeclaration(cancellationToken, out VariableDeclarationSyntax? declaration) &&
+                             declaration.Variables.TryLast(out var variable) &&
+                             variable.Initializer is { } initializer:
                         return TryGetCreation(initializer.Value, out nameArg) ||
                                TryGetCached(initializer.Value, semanticModel, cancellationToken, out nameArg);
                     case IMethodSymbol { Name: "GetOrAdd", ContainingType: { TypeArguments: { Length: 2 } typeArguments } } method
@@ -88,7 +93,7 @@
             return false;
         }
 
-        private static bool TryGetCreation(this ExpressionSyntax expression, [NotNullWhen(true)] out ArgumentSyntax? nameArg)
+        private static bool TryGetCreation(ExpressionSyntax expression, [NotNullWhen(true)] out ArgumentSyntax? nameArg)
         {
             nameArg = null;
             return expression is ObjectCreationExpressionSyntax { Type: { } type, ArgumentList: { Arguments: { Count: 1 } arguments } } &&
