32ce73ab227053a3afe38ba5fee68b0da3c1cbc1
JohanLarsson <johan_gavle1@hotmail.com>
2020-11-10 08:36:21 +0100

    struct PropertyNameArgument

---
+++ PropertyChangedAnalyzers/Helpers/PropertyNameArgument.cs
@@ -0,0 +1,26 @@
+﻿namespace PropertyChangedAnalyzers
+{
+    using System.Threading;
+    using Gu.Roslyn.AnalyzerExtensions;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    internal readonly struct PropertyNameArgument
+    {
+        internal readonly ArgumentSyntax? Argument;
+        internal readonly string? Name;
+
+        internal PropertyNameArgument(ArgumentSyntax? argument, string? name)
+        {
+            this.Argument = argument;
+            this.Name = name;
+        }
+
+        internal static PropertyNameArgument? Match(ArgumentSyntax argument, SemanticModel semanticModel, CancellationToken cancellationToken)
+        {
+            return argument.TryGetStringValue(semanticModel, cancellationToken, out var name)
+                ? new PropertyNameArgument(argument, name)
+                : (PropertyNameArgument?)null;
+        }
+    }
+}
