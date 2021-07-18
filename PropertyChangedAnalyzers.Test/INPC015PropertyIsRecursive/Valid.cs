019332e81e8a335cea0af52e82b91cc2552c02eb
jnm2 <me@jnm2.com>
2021-07-18 18:47:30 -0400

    Fix false recursion detection

--- PropertyChangedAnalyzers.Test/INPC015PropertyIsRecursive/Valid.cs
+++ PropertyChangedAnalyzers.Test/INPC015PropertyIsRecursive/Valid.cs
@@ -225,5 +225,72 @@ namespace ValidCode.Wrapping
 
             RoslynAssert.Valid(Analyzer, withProperties, code);
         }
+
+        [Test]
+        public static void SameExpressionWithDifferentMeanings()
+        {
+            var code = @"
+namespace ValidCode.Recursion
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class NotRecursion : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool Set<T>(ref T location, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (RuntimeHelpers.Equals(location, value)) return false;
+            location = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        record DifferentClass
+        {
+            public int P;
+        }
+
+        private int p;
+        public int P
+        {
+            get => p;
+            set
+            {
+                if (!Set(ref p, value)) return;
+
+                _ = new DifferentClass { P = value };
+                _ = new DifferentClass() with { P = value };
+
+                {
+                    int P;
+                    P = value;
+                }
+
+                _ = new Action<int>(P =>
+                {
+                    P = value;
+                });
+
+                LocalFunction(42);
+                void LocalFunction(int P)
+                {
+                    P = value;
+                }
+            }
+        }
+    }
+}
+";
+
+            RoslynAssert.Valid(Analyzer, code);
+        }
     }
 }
---
+++ ValidCode/Recursion/NotRecursion.cs
@@ -0,0 +1,58 @@
+﻿namespace ValidCode.Recursion
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class NotRecursion : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool Set<T>(ref T location, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (RuntimeHelpers.Equals(location, value)) return false;
+            location = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        record DifferentClass
+        {
+            public int P;
+        }
+
+        private int p;
+        public int P
+        {
+            get => p;
+            set
+            {
+                if (!Set(ref p, value)) return;
+
+                _ = new DifferentClass { P = value };
+                _ = new DifferentClass() with { P = value };
+
+                {
+                    int P;
+                    P = value;
+                }
+
+                _ = new Action<int>(P =>
+                {
+                    P = value;
+                });
+
+                LocalFunction(42);
+                void LocalFunction(int P)
+                {
+                    P = value;
+                }
+            }
+        }
+    }
+}
